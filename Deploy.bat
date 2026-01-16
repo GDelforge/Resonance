07650a1a509c7b946b7f8f731167eaa2df0d48de
GDelforge <gaspard.delforge@gmail.com>
2026-01-16 23:53:51 +0000

    The deploy skips all .* files and folder

--- Deploy.bat
+++ Deploy.bat
@@ -5,7 +5,7 @@ set "DEST=G:\My Drive\Data\Resonance"
 echo Deploying from "%SOURCE%" to "%DEST%"...
 
 :: Copy all content recursively (/E), excluding git/venv folders (/XD) and this script (/XF)
-robocopy "%SOURCE%." "%DEST%" /E /XD .git .venv __pycache__ /XF deploy.bat
+robocopy "%SOURCE%." "%DEST%" /E /XD .* __pycache__ /XF deploy.bat .*
 
 echo Done.
 pause
